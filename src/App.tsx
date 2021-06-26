573a2d81ec0f449d9793d96bdd1474a631c145a4
Joao Pedro Noronha <joaopdrons@gmail.com>
2021-06-25 21:26:11 -0300

    handleCreateRoom function

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from './context/AuthContext';
+import { Room } from './pages/Room';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
       </AuthContextProvider>
     </BrowserRouter>
   );
